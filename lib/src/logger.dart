a23058d155dd96248323662676f9f13040162442
Matan Lurey <matan@lurey.org>
2017-12-11 20:10:52 -0800

    Remove formatting.

--- lib/src/logger.dart
+++ lib/src/logger.dart
@@ -3,8 +3,6 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async';
-import 'dart:convert';
-import 'dart:io';
 
 import 'package:cable/cable.dart';
 import 'package:cable_stackdriver/cable_stackdriver.dart';
@@ -19,7 +17,7 @@ Future<T> initLogging<T>(
 }) async {
   return new Logger<Object>(
     destinations: [
-      const _JsonFormattedSink(),
+      LogSink.printSink,
       googleCloudKey.isNotEmpty
           ? await Stackdriver.serviceAccount<String>(
               googleCloudKey,
@@ -37,33 +35,4 @@ Future<T> initLogging<T>(
     );
     return run();
   });
-}
-
-class _JsonFormattedSink implements Sink<Record> {
-  const _JsonFormattedSink();
-
-  @override
-  void add(Record data) {
-    stdout.writeln(_formatter(data));
-  }
-
-  @override
-  void close() {}
-}
-
-const _json = const JsonEncoder.withIndent('  ');
-
-String _formatter(Record<Object> record) {
-  final timestamp = record.timestamp;
-  final timeFormat = new StringBuffer()
-    ..write(timestamp.hour)
-    ..write(':')
-    ..write(timestamp.minute)
-    ..write(':')
-    ..write(timestamp.second);
-  final name = record.origin != null ? ' ${record.origin}:' : '';
-  final payload = record.payload is Map
-      ? _json.convert(record.payload)
-      : record.payload.toString();
-  return '[${record.severity} @ $timeFormat]$name $payload';
 }
